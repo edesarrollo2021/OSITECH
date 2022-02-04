46b3c0fa6ddf592afe8d6fa3238d93a66be42139
edesarrollo <edesarrollo@ositesh.com.mx>
2022-02-04 13:46:53 -0400

    [IMP]Modificaciones

--- ot_website_slides/models/addresses.py
+++ ot_website_slides/models/addresses.py
@@ -5,10 +5,14 @@ from odoo.exceptions import UserError
 
 class Addresses(models.Model):
     _name = 'slide.addresses'
+    
+    def _compute_instructions(self):
+        self.instructions = 'hola mundo 1'
 
     name = fields.Char("Direcciones", required=True)
     image = fields.Binary(string="Imagen", required=True)
     job_positions_ids = fields.One2many('slide.job.positions', 'addresses_id', string="Puestos de trabajo", required=True)
+    instructions = fields.Text(string='Instrucciones', compute='_compute_instructions', readonly=True)
 
 class JobPositions(models.Model):
     _name = 'slide.job.positions'
