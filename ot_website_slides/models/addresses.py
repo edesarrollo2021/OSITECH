4574ffb97de72413dee025cac7128c038e108242
edesarrollo <edesarrollo@ositesh.com.mx>
2022-02-08 13:23:58 -0400

    [IMP]Modificaciones

--- ot_website_slides/models/addresses.py
+++ ot_website_slides/models/addresses.py
@@ -6,13 +6,12 @@ from odoo.exceptions import UserError
 class Addresses(models.Model):
     _name = 'slide.addresses'
     
-    def _compute_instructions(self):
-        self.instructions = 'hola mundo 1'
+    # ~ def _compute_instructions(self):
+        # ~ self.instructions = 'hola mundo 1'
 
     direction_id = fields.Many2one('hr.department', string="Direcciones", domain=[('elearning', '=', True)])
     image = fields.Binary(string="Imagen", required=True)
     job_positions_ids = fields.One2many('slide.job.positions', 'addresses_id', string="Puestos de trabajo", required=True)
-    instructions = fields.Text(string='Instrucciones', compute='_compute_instructions', readonly=True)
 
 class JobPositions(models.Model):
     _name = 'slide.job.positions'
@@ -21,6 +20,6 @@ class JobPositions(models.Model):
     job_id = fields.Many2one('hr.job', string="Puestos de trabajo", required=True)
     image = fields.Binary(string="Imagen", required=True)
     addresses_id = fields.Many2one('slide.addresses', string="Direcciones")
-    order_by = fields.Float("Order")
+    order_by = fields.Float("Orden")
     enroll_group_id = fields.Many2one('res.groups', string='Suscribir grupo', required=True)
     
