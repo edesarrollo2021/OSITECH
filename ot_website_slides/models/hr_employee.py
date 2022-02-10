0e8640f3818d33a658645e0bb046fcef51c24b9f
edesarrollo <edesarrollo@ositesh.com.mx>
2022-02-10 19:06:59 -0400

    [IMP]Modificaciones

--- ot_website_slides/models/hr_employee.py
+++ ot_website_slides/models/hr_employee.py
@@ -11,11 +11,18 @@ class HrEmployeePrivate(models.Model):
     
     def write(self, vals):
         if "job_id" in vals:
-            print ("vals", vals)
             user_id = self.env['res.users'].sudo().search([('login', '=', self.work_email)], limit=1)
             slide_job = self.env['slide.job.positions'].sudo().search([('job_id', '=', vals.get("job_id"))], limit=1)
             group = slide_job.enroll_group_id
             group.write({'users': [[6, False, [user_id.id]]]})
+        if "active" in vals:
+            print("AAAAA")
+            if not vals.get("active"):
+                user = self.user_id
+                user.update({"active":False})
+            elif vals.get("active"):
+                user = self.user_id
+                user.update({"active":True})
         return super(HrEmployeePrivate, self).write(vals)
 
   
